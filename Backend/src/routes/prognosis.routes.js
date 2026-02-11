74e501d585c432f479b8f2792b407d5c8f48fdea
rrrsahil <163717960+rrrsahil@users.noreply.github.com>
2026-02-11 15:08:12 +0530

    Delete Backend/src directory

--- Backend/src/routes/prognosis.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { predictQuality, getPrognosisHistory } from "../controllers/prognosis.controller.js";
-
-const router = express.Router();
-
-router.post("/predict", predictQuality);
-router.get("/history", getPrognosisHistory);
-
-export default router;
